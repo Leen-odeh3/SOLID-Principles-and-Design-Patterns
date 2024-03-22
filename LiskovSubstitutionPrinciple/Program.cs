05e52bb7d09d41c0be2446bc049b24e732578027
Leen-odeh3 <123558998+Leen-odeh3@users.noreply.github.com>
2024-03-22 18:07:10 +0300

    Alter Program.cs

--- LiskovSubstitutionPrinciple/Program.cs
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -3,4 +3,4 @@
 // Defition: A child/sub/derived class should be completely substitutable to it's base or parent class. This principle is very similar to the
 // Interface Segration Principle.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello World!");
